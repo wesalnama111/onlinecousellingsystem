9c4b664288b17af8a4bac81b36fa50b991525604
wesalnama111 <wesal.iintellect@gmail.com>
2018-02-05 19:26:36 +0500

    done intill dynamic content

--- src/App.js
+++ src/App.js
@@ -7,40 +7,74 @@ class App extends Component {
       { Name:'max', age:28 },
       { Name:'menu', age:27 },
       { Name:'wesal',age:29 },
-      { Name:'zindagi',age:100 }
+      
     ]
   }
-  SwitchButton=()=>{
+  SwitchButton=(nName)=>{
+    
     this.setState({
        persons:[
-      { Name:'maximize', age:28 },
+      { Name:'wesal', age:28 },
       { Name:'linkedIn', age:27 },
       { Name:'wesalia',age:32 },
-      { Name:'death',age:100 }
+      
       
     ]})
   }
-  render() {
-    return (
-      <div className="App">
-        <h1>hello this is the change</h1>
-        <button onClick={this.SwitchButton}>switch Name</button> 
+  nameChanger=(event)=>{
+    this.setState({
+      persons:[
+     { Name:'wesal', age:28 },
+     { Name:event.target.value, age:27 },
+     { Name:event.target.value,age:32 },
+   ],
+  otherState:'someother values',
+  showPersons: false
+  })
+  }
+  togglePerson=()=>{
 
+    const doesShow =this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+  render() {
+    const style={
+      backgroundColor: 'black',
+      font:'inherit',
+      border:'1px solid blue',
+      padding :'8px',
+      cursor:'pointer',
+      color: 'white'
+
+    }
+    let person =null;
+    if (this.state.showPersons){
+      person= (
+        <div>
         <Person 
         Name={this.state.persons[0].Name} 
         age={this.state.persons[0].age} />
         <Person 
-        Name={this.state.persons[1].Name} 
-      
-        age={this.state.persons[1].age} >
+        Name={this.state.persons[1].Name}
+        age={this.state.persons[1].age} 
+        click={()=>this.SwitchButton('lasew')}
+        changeto={this.nameChanger}>
         Hobbies:cricket
         </Person>
         <Person 
         Name={this.state.persons[2].Name} 
         age={this.state.persons[2].age}/>
-        <Person 
-        Name={this.state.persons[3].Name} 
-        age={this.state.persons[3].age}/>
+        
+        </div>
+      
+
+       ) }
+    
+    return (
+      <div className="App">
+        <h1>My first react App</h1>
+        <button style={style} onClick={this.togglePerson}>show the details</button> 
+    {person}
         
       </div>
     );
