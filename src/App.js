b8e7222ed2fc0f2f73fca9cd5d04ac6923bc61db
wesalnama111 <wesal.iintellect@gmail.com>
2018-02-08 19:08:46 +0500

    done till section 5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium, { StyleRoot} from 'radium';
+
 class App extends Component {
   state = {
     persons:
     [
       { id:'1st', name:'max', age:28 },
       { id:'2nd', name:'menu', age:27 },
-      { id:'3rd', name:'wesal', age:29 }
-      
-    ]
+      { id:'3rd', name:'wesal', age:29 } 
+    ],
+    otherState:'someother values',
+    showPersons: false
   }
+
   nameChange = ( event, id ) => {
     const personIndex = this.state.persons.findIndex( p => {
       return p.id === id;
@@ -20,6 +24,22 @@ class App extends Component {
     };
      // const person=Object.assign({},this.state.persons[personIndex]);
     person.name = event.target.value;
+    
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({ persons: persons });
+  }
+
+  nameChanged = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex( p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+     // const person=Object.assign({},this.state.persons[personIndex]);
+   
+    person.age= event.target.value;
     const persons = [...this.state.persons];
     persons[personIndex] = person;
     this.setState({ persons: persons });
@@ -48,6 +68,7 @@ class App extends Component {
     const doesShow =this.state.showPersons;
     this.setState({showPersons: !doesShow});
   }
+
   render() {
     const style = {
       backgroundColor: 'black',
@@ -55,7 +76,11 @@ class App extends Component {
       border:'1px solid blue',
       padding :'8px',
       cursor:'pointer',
-      color: 'white'
+      color: 'white',
+      ':hover':{
+        backgroundColor:'white',
+        color:'black'
+      }
     }
     let person = null;
     if (this.state.showPersons)
@@ -69,23 +94,43 @@ class App extends Component {
                         name = {persone.name} 
                         age = {persone.age}
                         key = {persone.id}
-                        changed = {(event) => this.nameChange(event, persone.id)}/> 
+                        changed = {(event) => this.nameChange(event, persone.id)}
+                        change = {(event) => this.nameChanged(event, persone.id)}
+                        /> 
               })}
         </div>
       ) 
+      style.backgroundColor='white';
+      style.color='black';
+      
+      style[':hover'] = {
+        backgroundColor:'black',
+        color:'white'
+      }
     }
 
-    
+    const classes = [];
+    if(this.state.persons.length <= 2 ){
+      classes.push('red');
+    }
+
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
+    }
+
     return (
+      <StyleRoot>
       <div className="App">
         <h1>My first react App</h1>
+        <p className={classes.join(' ')}>it works really fine </p>
         <button style = {style} onClick = {this.togglePerson}>show the details</button> 
         {person}
         
       </div>
+      </StyleRoot>
     );
   // return React.createElement('div', {className:'App'}, React.createElement('h1',null,'does thiswork now'));
   }
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './person.css';
+import Radium from 'radium';
 const person = (props) =>{
+    const style = newFunction();
     return (
-        <div className="person">
-         <p onClick={props.click}> i'm {props.name} and i'm  {props.age} years old.</p>
+        <div className="person" style={style}>
+         <p onClick={props.click}> {props.name} {props.age}</p>
         <p> {props.children} </p>
-        <input type="test" onChange={props.changed} />
+        <input type="text" onChange={props.changed} />
+        <input type="text" onChange={props.change} />
+        
         </div>
 )
 };
-export default person;
+
+export default Radium(person);
+
+function newFunction() {
+    const style = {
+        '@media (min-width:500px)': {
+            width: '450px'
+        }
+    };
+    return style;
+}
